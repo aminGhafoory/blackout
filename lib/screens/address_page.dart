ccaf7f395fe092b486bb66b2d0b9f05131eac9eb
aminGhafoory <74732606+aminGhafoory@users.noreply.github.com>
2025-05-25 18:49:11 +0330

    minor changes nothing important

---
+++ lib/screens/address_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class AddressPage extends StatelessWidget {
+  const AddressPage({super.key, required this.address});
+  final String? address;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Address",
+          textDirection: TextDirection.rtl,
+        ),
+      ),
+      body: SingleChildScrollView(
+          child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text(
+              style: TextStyle(
+                  fontFamily: "Vazirmatn", fontWeight: FontWeight.bold),
+              "آدرس",
+              textDirection: TextDirection.rtl,
+            ),
+            Text(
+              style: TextStyle(fontFamily: "Vazirmatn"),
+              address ?? "",
+              textDirection: TextDirection.rtl,
+            ),
+          ],
+        ),
+      )),
+    );
+  }
+}
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:blackout/models/userperfs.dart';
 import 'package:blackout/providers/blackout_provider.dart';
 import 'package:blackout/providers/user_perfs_provider.dart';
+import 'package:blackout/screens/address_page.dart';
 import 'package:blackout/screens/data_entry_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -56,98 +57,120 @@ class HomePage extends ConsumerWidget {
           return ListView.builder(
             itemCount: data.data.length,
             itemBuilder: (context, index) {
-              return Container(
-                  padding: EdgeInsets.all(12),
-                  margin: EdgeInsets.all(4),
-                  decoration: BoxDecoration(
-                      color: const Color.fromARGB(118, 105, 240, 175),
-                      borderRadius: BorderRadius.circular(8)),
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            children: [
-                              Icon(Icons.calendar_month_outlined, size: 20),
-                              Text(data.data[index].outageDate,
-                                  style: TextStyle(fontWeight: FontWeight.bold))
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Text(
-                                '${data.data[index].outageStartTime} - ${data.data[index].outageStopTime}',
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                              SizedBox(width: 2),
-                            ],
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Column(
-                        children: [
-                          Directionality(
-                            textDirection: TextDirection.rtl,
-                            child: Row(
-                              children: [
-                                Icon(Icons.manage_accounts, size: 20),
-                                SizedBox(
-                                  width: 2,
-                                ),
-                                Text(
-                                    style: TextStyle(fontFamily: "Vazirmatn"),
-                                    "ثبت کننده : ${data.data[index].registrar}"),
-                              ],
-                            ),
-                          ),
-                          Directionality(
-                            textDirection: TextDirection.rtl,
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: [
-                                Icon(Icons.event_available_outlined, size: 20),
-                                SizedBox(width: 2),
-                                Text(
-                                  "برنامه ریزی شده : ",
-                                  style: TextStyle(fontFamily: "Vazirmatn"),
-                                ),
-                                Text(
-                                  style: TextStyle(fontFamily: "Vazirmatn"),
-                                  data.data[index].isPlanned ? "بله" : "خیر",
-                                )
-                              ],
-                            ),
-                          ),
-                          Directionality(
-                            textDirection: TextDirection.rtl,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Row(
-                                  children: [
-                                    Icon(Icons.settings_applications_sharp,
-                                        size: 20),
-                                    SizedBox(width: 2),
-                                    Text(
-                                        style:
-                                            TextStyle(fontFamily: "Vazirmatn"),
-                                        "علت: ${data.data[index].reasonOutage}"),
-                                  ],
-                                ),
-                                Text(
-                                    style: TextStyle(fontFamily: "Vazirmatn"),
-                                    "تاریخ ثبت : ${data.data[index].regDate}"),
-                              ],
-                            ),
-                          )
-                        ],
-                      )
-                    ],
+              return GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) =>
+                        AddressPage(address: data.data[index].address),
                   ));
+                },
+                child: Container(
+                    padding: EdgeInsets.all(12),
+                    margin: EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                        color: const Color.fromARGB(118, 105, 240, 175),
+                        borderRadius: BorderRadius.circular(8)),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Icon(Icons.calendar_month_outlined, size: 20),
+                                Text(data.data[index].outageDate,
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold))
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  '${data.data[index].outageStartTime} - ${data.data[index].outageStopTime}',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(width: 2),
+                              ],
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Column(
+                          children: [
+                            Directionality(
+                              textDirection: TextDirection.rtl,
+                              child: Row(
+                                children: [
+                                  Icon(Icons.manage_accounts, size: 20),
+                                  SizedBox(
+                                    width: 2,
+                                  ),
+                                  Text(
+                                      style: TextStyle(fontFamily: "Vazirmatn"),
+                                      "ثبت کننده : ${data.data[index].registrar}"),
+                                ],
+                              ),
+                            ),
+                            Directionality(
+                              textDirection: TextDirection.rtl,
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  Icon(Icons.event_available_outlined,
+                                      size: 20),
+                                  SizedBox(width: 2),
+                                  Text(
+                                    "برنامه ریزی شده : ",
+                                    style: TextStyle(fontFamily: "Vazirmatn"),
+                                  ),
+                                  Text(
+                                    style: TextStyle(fontFamily: "Vazirmatn"),
+                                    data.data[index].isPlanned ? "بله" : "خیر",
+                                  )
+                                ],
+                              ),
+                            ),
+                            Directionality(
+                              textDirection: TextDirection.rtl,
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
+                                    children: [
+                                      Icon(Icons.settings_applications_sharp,
+                                          size: 20),
+                                      SizedBox(width: 2),
+                                      Text(
+                                          style: TextStyle(
+                                              fontFamily: "Vazirmatn"),
+                                          "علت: ${data.data[index].reasonOutage}"),
+                                    ],
+                                  ),
+                                  Text(
+                                      style: TextStyle(fontFamily: "Vazirmatn"),
+                                      "تاریخ ثبت : ${data.data[index].regDate}"),
+                                ],
+                              ),
+                            ),
+                            Directionality(
+                              textDirection: TextDirection.rtl,
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  data.data[index].address.contains("كهل")
+                                      ? Icon(Icons.square_rounded, size: 20)
+                                      : SizedBox(width: 0, height: 0)
+                                ],
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    )),
+              );
             },
           );
         },
